5ad2cd7004418b254077533f4610add7343c5cc8
accustraswaraj <66258051+accustraswaraj@users.noreply.github.com>
2025-04-18 12:21:26 +0530

    Update index.ts

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,10 @@
 import { Hono } from "hono";
 const app = new Hono<{ Bindings: Env }>();
 
+
+// Set the base path
+app.basePath('/base')
+
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
 
 export default app;
